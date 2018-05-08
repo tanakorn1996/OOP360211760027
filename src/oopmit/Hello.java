a551da17e111227775c9936a7ad8dfd3d17411ed
Thanakorn Phaothong <tanakorn46034603@gmail.com>
2018-05-08 15:26:54 +0700

    update data

---
+++ src/oopmit/Hello.java
@@ -0,0 +1,11 @@
+package oopmit;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello,My name is Thanakorn");
+        System.out.println("Major: Information System");
+        System.out.println("Faculy: Management Technology");
+        System.out.println("University: RMUTSV");
+    }
+
+}
