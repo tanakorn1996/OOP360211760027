de803de1bf388d6b279084c6dc16a7f1968d01f1
Thanakorn Phaothg <tanakorn46034603@gmail.com>
2018-05-09 10:13:12 +0700

    Array Operator

--- src/lab5/myArray.java
+++ src/lab5/myArray.java
@@ -3,33 +3,33 @@ package lab5;
 import java.util.Scanner;
 
 public class myArray {
-    public static void main(String[] args) {
-        // array
+  public static void main(String[] args) {
+    // array
 
-        int a[] = new int[5];
-        System.out.println(a[0]);
-        a[0] = 10;
-        System.out.println(a[0]);
-        //input data to array
-        Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < a.length; i++) {
-            System.out.print("a["+i+"]: ");
-            a[i] = sc.nextInt();
-        }
+    int a[] = new int[5];
+    System.out.println(a[0]);
+    a[0] = 10;
+    System.out.println(a[0]);
+    //input data to array
+    Scanner sc = new Scanner(System.in);
+    for (int i = 0; i < a.length; i++) {
+      System.out.print("a["+i+"]: ");
+      a[i] = sc.nextInt();
+    }
 
-        //show data in array
-        System.out.println("Data in array:");
-        for (int i = 0; i < a.length; i++) {
-            System.out.print(a[i]+"\t");
-
-        }
-        System.out.println("\nEnhanced for Loop");
-        //enhanced for loop
-        for (int val:a){
-            System.out.print(val+"\t");
-        }
-
-
+    //show data in array
+    System.out.println("Data in array:");
+    for (int i = 0; i < a.length; i++) {
+      System.out.print(a[i]+"\t");
 
     }
+    System.out.println("\nEnhanced for Loop");
+    //enhanced for loop
+    for (int val:a){
+      System.out.print(val+"\t");
+    }
+
+
+
+  }
 }//class
